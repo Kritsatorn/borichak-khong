88482942d93d40607fe18fb4f89fe1b6a7573b8b
Kritsatorn Saengweang <kritsatornsaengweang@MacBook-Pro-4.local>
2021-02-23 17:25:32 +0700

    added

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-const Donator = ({ setThings, TrackCode, setTrackCode }) => {
+const Donator = ({ setThings, TrackCode, setTrackCode, Process }) => {
   const [textInput, setTextInput] = useState("");
   const onInputSubmit = (event) => {
     if (event) {
@@ -7,10 +7,9 @@ const Donator = ({ setThings, TrackCode, setTrackCode }) => {
     }
     setThings(textInput);
     setTrackCode("1234");
-    console.log(TrackCode);
   };
   return (
-    <div>
+    <div className="flex flex-col justify-center">
       <header className="mt-8 text-center text-blue-300 font-black text-3xl">
         Donator Section
       </header>
@@ -32,11 +31,21 @@ const Donator = ({ setThings, TrackCode, setTrackCode }) => {
         Track Code :  ${TrackCode}`
           : null}
       </div>
+      <div className="mt-4 text-center font-bold text-2xl text-indigo-400">
+        {Process === 1 ? "Horay Your things is Accepted , Let's share" : null}
+
+        {Process === 2 ? " Sorry Your Things is rejected" : null}
+      </div>
+      {Process === 1 && (
+        <button className="mt-4 mx-auto py-3 px-5 rounded-3xl bg-purple-600 text-gray-100 text-xl  uppercase ">
+          Share
+        </button>
+      )}
     </div>
   );
 };
 
-const DonateTaker = ({ Things, TrackCode }) => {
+const DonateTaker = ({ Things, TrackCode, setProcess, Process }) => {
   const [textInput, setTextInput] = useState("");
   const [open, setOpen] = useState(false);
   const onInputSubmit = (event) => {
@@ -67,6 +76,25 @@ const DonateTaker = ({ Things, TrackCode }) => {
       <div className="mt-4 text-center font-bold text-2xl text-green-700">
         {open ? ` Hooray this is Things :   ` : null}
         <span className="text-pink-500"> {open ? ` ${Things}  ` : null} </span>
+        <div className="mt-4 space-x-20 text-gray-600">
+          <button
+            className="py-3 px-5 rounded-3xl bg-pink-200 uppercase"
+            onClick={() => setProcess(1)}
+          >
+            {" "}
+            Accept{" "}
+          </button>
+          <button
+            className="py-3 px-5 rounded-3xl bg-indigo-200 uppercase"
+            onClick={() => setProcess(2)}
+          >
+            {" "}
+            Reject{" "}
+          </button>
+        </div>
+        <div className="mt-4 text-red-600 text-lg">
+          {Process === 1 || Process === 2 ? "Hooray Process has END !" : null}
+        </div>
       </div>
     </div>
   );
@@ -76,6 +104,7 @@ export default function HomePage() {
   const [Who, setWHo] = useState(0);
   const [Things, setThings] = useState("");
   const [TrackCode, setTrackCode] = useState("");
+  const [Process, setProcess] = useState(null);
   const handleClick = (who) => {
     setWHo(who);
   };
@@ -110,9 +139,17 @@ export default function HomePage() {
           setThings={setThings}
           TrackCode={TrackCode}
           setTrackCode={setTrackCode}
+          Process={Process}
         />
       )}
-      {Who === 2 && <DonateTaker Things={Things} TrackCode={TrackCode} />}
+      {Who === 2 && (
+        <DonateTaker
+          Things={Things}
+          TrackCode={TrackCode}
+          setProcess={setProcess}
+          Process={Process}
+        />
+      )}
     </div>
   );
 }
